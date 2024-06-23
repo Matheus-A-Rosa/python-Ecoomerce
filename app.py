d2447cf73eff71206078da1a25fbb97ffe5d406a
Matheus Rosa <matheus_a_rosa@hotmail.com>
2024-06-23 16:48:22 -0300

    inicial config

---
+++ app.py
@@ -0,0 +1,13 @@
+# Importação
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+# Definir uma rota raiz (página inicial) e a função que será executada ao requisitar
+@app.route('/teste')
+def hello_world():
+    return 'Hello World'
+
+if __name__ == "__main__":
+    app.run(debug=True) 
